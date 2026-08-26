b91084612eb94904d761a3efdf6171e92d3ae8b0
Nick Aspenwall <nick.aspenwall@cloud-team.com>
2026-08-26 16:53:48 -0500

    feat(backup): add backuponly flag

--- cmd/db/spanner/stagedb/stagedb.go
+++ cmd/db/spanner/stagedb/stagedb.go
@@ -18,6 +18,7 @@ func Command(ctx context.Context) *cobra.Command {
 }
 
 func setup(ctx context.Context) *cobra.Command {
+	var backupOnly bool
 	cmd := &cobra.Command{
 		Use:   "stagedb [target]",
 		Short: "Back up and restore given source Spanner database to provided target database",
@@ -25,7 +26,7 @@ func setup(ctx context.Context) *cobra.Command {
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			target := args[0]
-			if err := run(ctx, target); err != nil {
+			if err := run(ctx, target, cmd); err != nil {
 				return errors.Wrap(err, "run()")
 			}
 
@@ -33,18 +34,46 @@ func setup(ctx context.Context) *cobra.Command {
 		},
 	}
 
+	cmd.Flags().BoolVarP(&backupOnly, "backupOnly", "b", false, "Only execute backup")
+
 	return cmd
 }
 
-func run(ctx context.Context, target string) error {
+func run(ctx context.Context, target string, cmd *cobra.Command) error {
 	db, err := newConfig(ctx)
 	if err != nil {
 		return errors.Wrap(err, "failed to initialize config")
 	}
-	defer db.spanner.Close()
+	defer func() {
+		if err := db.spanner.Close(); err != nil {
+			log.Println("error closing database")
+		}
+	}()
+
+	backupOnly, err := cmd.Flags().GetBool("backupOnly")
+	if err != nil {
+		return errors.Wrap(err, "run()")
+	}
+	if backupOnly {
+		log.Println("backup only flag received, will not restore")
+		if err := backup(ctx, db, target); err != nil {
+			return err
+		}
+
+		return nil
+	}
 
 	if err := backupRestore(ctx, db, target); err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func backup(ctx context.Context, db *config, target string) error {
+	_, err := db.spanner.Backup(ctx)
+	if err != nil {
+		return errors.Wrap(err, "backup()")
 	}
 
 	return nil
